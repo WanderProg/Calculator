116ee0477d8fbbddb4149f4c6cfc084c9d2e7ad7
Wander van Leeuwen <wanderleeuwenvan@gmail.com>
2024-08-11 02:09:52 +0200

    create basic HTML and CSS makeup with all the buttons

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const number1 = ""
+const number2 = ""
+const operator = ""
+
 function sum(num1, num2) {
     return num1 + num2
 }
@@ -10,7 +14,24 @@ function multiply(num1, num2) {
     return num1 * num2
 }
 
-function devide(num1, num2) {
+function divide(num1, num2) {
     return num1 / num2
 }
 
+function operate (operator, number1, number2) {
+    switch (operator) {
+    case '+':
+    return sum(number1, number2)
+    break;
+    case '-':
+    return subtract(number1, number2)
+    break;
+    case '*':
+    return multiply(number1, number2)
+    break;
+    case '/':
+    return divide(number1, number2)
+    }
+    }
+
+    console.log(operate("+", 10, 15))
