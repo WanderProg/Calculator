a972fe5e54369689e4516edead7cba9f7a7f873a
Wander van Leeuwen <wanderleeuwenvan@gmail.com>
2024-08-28 14:16:38 +0200

    add all buttons, make everything work except edge-cases

--- script.js
+++ script.js
@@ -15,57 +15,176 @@ const one = document.querySelector(".one")
 const two = document.querySelector(".two")
 const three = document.querySelector(".three")
 const plus = document.querySelector(".plus")
-const zer0 = document.querySelector(".zer0")
+const zero = document.querySelector(".zero")
 const dot = document.querySelector(".dot")
 const is = document.querySelector(".is")
 
-
 ac.addEventListener("click", () => {
-    display.innerHTML = " "
+  display.value = ""
 })
 
 seven.addEventListener("click", () => {
-    const sevenIn = 7;
-    display.innerHTML += sevenIn; // This appends the number 7 to the existing content in display
-});
+  display.value += 7
+})
 
-console.log(modules)
+eight.addEventListener("click", () => {
+  display.value += 8
+})
 
+nine.addEventListener("click", () => {
+  display.value += 9
+})
+
+six.addEventListener("click", () => {
+  display.value += 6
+})
+
+five.addEventListener("click", () => {
+  display.value += 5
+})
+
+four.addEventListener("click", () => {
+  display.value += 4
+})
+
+three.addEventListener("click", () => {
+  display.value += 3
+})
+
+two.addEventListener("click", () => {
+  display.value += 2
+})
+
+one.addEventListener("click", () => {
+  display.value += 1
+})
+
+zero.addEventListener("click", () => {
+  display.value += 0
+})
+plus.addEventListener("click", () => {
+  if (
+    display.value.includes("+") ||
+    display.value.includes("-") ||
+    display.value.includes("*") ||
+    display.value.includes("/")
+  ) {
+     display.value = calculateData(display.value)
+  }
+  display.value += "+"
+})
+
+minus.addEventListener("click", () => {
+  if (
+    display.value.includes("+") ||
+    display.value.includes("-") ||
+    display.value.includes("*") ||
+    display.value.includes("/")
+  ) {
+     display.value = calculateData(display.value)
+  }
+  display.value += "-"
+})
+
+divides.addEventListener("click", () => {
+  if (
+    display.value.includes("+") ||
+    display.value.includes("-") ||
+    display.value.includes("*") ||
+    display.value.includes("/")
+  ) {
+     display.value = calculateData(display.value)
+  }
+  display.value += "/"
+})
+
+multiplyer.addEventListener("click", () => {
+  if (
+    display.value.includes("+") ||
+    display.value.includes("-") ||
+    display.value.includes("*") ||
+    display.value.includes("/")
+  ) {
+     display.value = calculateData(display.value)
+    console.log(display.value)
+  }
+  display.value += "*"
+})
+
+is.addEventListener("click", () => {
+  display.value = calculateData(display.value)
+})
 
 const number1 = ""
 const number2 = ""
 const operator = ""
 
 function add(num1, num2) {
-    return num1 + num2
+  return num1 + num2
 }
 
 function subtract(num1, num2) {
-    return num1 - num2
+  return num1 - num2
 }
 
 function multiply(num1, num2) {
-    return num1 * num2
+  return num1 * num2
 }
 
 function divide(num1, num2) {
-    return num1 / num2
+  return num1 / num2
 }
 
-function operate (operator, number1, number2) {
-    switch (operator) {
-    case '+':
-    return add(number1, number2)
-    break;
-    case '-':
-    return subtract(number1, number2)
-    break;
-    case '*':
-    return multiply(number1, number2)
-    break;
-    case '/':
-    return divide(number1, number2)
-    }
-    }
+function operate(operator, number1, number2) {
+  switch (operator) {
+    case "+":
+      return add(number1, number2)
+      break
+    case "-":
+      return subtract(number1, number2)
+      break
+    case "*":
+      return multiply(number1, number2)
+      break
+    case "/":
+      return divide(number1, number2)
+  }
+}
 
-    console.log(operate("+", 10, 15))
+function cantHandle() {
+  return "Can't devive"
+
+}
+
+function calculateData(calculatorString) {
+  let currentValue = calculatorString
+
+  let firstValue = ""
+  let operator = ""
+  let secondValue = ""
+
+  for (i = 0; i < currentValue.length; i++) {
+    if (
+      currentValue[i] === "+" ||
+      currentValue[i] === "-" ||
+      currentValue[i] === "*" ||
+      currentValue[i] === "/"
+    ) {
+      operator = currentValue[i]
+    } else {
+      if (operator == "") {
+        firstValue = firstValue + currentValue[i]
+      } else {
+        secondValue = secondValue + currentValue[i]
+      }
+    }
+  }
+
+  if(operator === "/" && secondValue.includes(0)) {
+    // return "Can't devide"
+    return cantHandle()
+  }
+
+
+  return operate(operator, Number(firstValue), Number(secondValue))
+}
